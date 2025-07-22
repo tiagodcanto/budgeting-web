0d49bcb16862f4fe5193657cb92e53f5bf283240
Tiago do Canto <tiago.d.canto@gmail.com>
2025-07-22 10:32:04 +0200

    new form design

--- src/app/main/home/home.component.ts
+++ src/app/main/home/home.component.ts
@@ -4,10 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterLink, MatButtonModule, MatCardModule, HeaderComponent],
+  imports: [CommonModule, RouterLink, MatButtonModule, MatCardModule, HeaderComponent, MatInputModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
